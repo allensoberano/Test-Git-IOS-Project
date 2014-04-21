0213e8682ea8e4d0614f0afe73c4d6aff206aac6
Allen Soberano <Allen@Allens-MacBook-Pro.local>
2014-04-21 00:00:13 -0700

    added a test

---
+++ Git Test/ASTest.h
@@ -0,0 +1,13 @@
+//
+//  ASTest.h
+//  Git Test
+//
+//  Created by Allen Soberano on 4/20/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASTest : NSObject
+
+@end
---
+++ Git Test/ASTest.m
@@ -0,0 +1,13 @@
+//
+//  ASTest.m
+//  Git Test
+//
+//  Created by Allen Soberano on 4/20/14.
+//
+//
+
+#import "ASTest.h"
+
+@implementation ASTest
+
+@end
